9958d10c9140712498ac09e6e6a15fbf0c0b41a5
Harrison Runnels <h.runnels3@gmail.com>
2018-10-10 11:02:15 -0500

    "updated git with manifest changes"

--- S.P.U.D.Main/popup.js
+++ S.P.U.D.Main/popup.js
@@ -1,9 +1,13 @@
 
-function setBadge(){chrome.browserAction.setBadgeBackgroundColor({ 'color': "#777" });
-chrome.browserAction.setBadgeText({'text': "Hello"});}
+function setBadge(str) {
+    chrome.browserAction.setBadgeBackgroundColor({ 'color': "#777" });
+    chrome.browserAction.setBadgeText({'text': str});
+}
 
 chrome.history.onVisited.addListener(function(result) {
     if (result.url == "https://www.youtube.com/") {
-        setBadge();
+        setBadge("YAY");
     }
 }); 
+
+setBadge("Hello");
