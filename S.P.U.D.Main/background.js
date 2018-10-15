31c998e547af42b9562dc722da02180b6e63a7e5
Harrison Runnels <h.runnels3@gmail.com>
2018-10-14 21:54:22 -0500

    Added notifications and other functions for background and popup

--- S.P.U.D.Main/background.js
+++ S.P.U.D.Main/background.js
@@ -1,3 +1,16 @@
+// things that run in background
+
+// ---------------------- START OF FUNCTIONS ---------------------
+
+// sets the icon badge to a given text and color
+var red = "#FF0000"
+var grey = "#777"
+function setBadge(str, color) {
+    chrome.browserAction.setBadgeBackgroundColor({ 'color': color });
+    chrome.browserAction.setBadgeText({'text': str});
+}
+
+// listens to see if a certain url appears in the history 
 function checkVisited() {//checks youtube only currently
     chrome.history.onVisited.addListener(function(result) {
         if (result.url == "https://www.youtube.com/") {
@@ -5,10 +18,29 @@ function checkVisited() {//checks youtube only currently
         }
     }); 
 }
-var red = "#FF0000"
-function setBadge(str, color) {
-    chrome.browserAction.setBadgeBackgroundColor({ 'color': color });
-    chrome.browserAction.setBadgeText({'text': str});
-}
 
-checkVisited();
+// Creates notification settings, in future we could have a random generator that points to
+// title/message combination ie. num[0] = title and num[0[0]] = message or something like that
+var note = {
+    type: "basic",
+    title: "You boy.",
+    message: "Why you no listen?",
+    iconUrl: "icon_128.png"
+  }
+function notify(){
+    chrome.notifications.create(null, note, null)
+}
+//
+
+// ---------------------- END OF FUNCTIONS ---------------------
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab ) {
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+    var url = tabs[0].url;
+    if (url == "https://www.youtube.com/"){
+        //alert("bonk")
+      }
+   }
+);
+});
--- S.P.U.D.Main/popup.js
+++ S.P.U.D.Main/popup.js
@@ -5,3 +5,28 @@ function setBadge(str) {
 }
 
 //setBadge("Hello");
+
+// ** Creates notification settings, in future we could have a random generator that points to
+// ** title/message combination ie. num[0] = title and num[0[0]] = message or something like that
+var note = {
+    type: "basic",
+    title: "You boy.",
+    message: "Why you no listen?",
+    iconUrl: "icon_128.png"
+  }
+function notify(){
+    chrome.notifications.create(null, note, null)
+}
+// ****
+
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+   function(tabs){
+      url = tabs[0].url;
+      if (url == "https://www.youtube.com/"){
+        notify();
+      }
+   }
+);
+
+
+
