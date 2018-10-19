5689850bf1c1b9186dd770d59c227f9ab07af43a
antonygor <agor@macalester.edu>
2018-10-19 09:33:35 -0500

    Mid-Term Demo (HTML)

--- S.P.U.D.Main/popup.js
+++ S.P.U.D.Main/popup.js
@@ -1,32 +1,21 @@
+// import swal from 'sweetalert';
 
-function setBadge(str) {
-    chrome.browserAction.setBadgeBackgroundColor({ 'color': "#777" });
-    chrome.browserAction.setBadgeText({'text': str});
+function addWebsite() {
+    // alert("website added");
+    // swal("hello world");
+    var txt;
+    if (confirm("add this website?")){
+        txt = "Website Added"
+    }else{
+        txt = "Website Not Added"
+    }
+        document.getElementById("demo").innerHTML = txt
+    
+    // document.getElementById("demo").innerHTML="We will write js code to add a website";
 }
-
-//setBadge("Hello");
-
-// ** Creates notification settings, in future we could have a random generator that points to
-// ** title/message combination ie. num[0] = title and num[0[0]] = message or something like that
-var note = {
-    type: "basic",
-    title: "You boy.",
-    message: "Why you no listen?",
-    iconUrl: "icon_128.png"
-  }
-function notify(){
-    chrome.notifications.create(null, note, null)
+ 
+function closeAlert(){
+    document.getElementsByClassName('green alert').this.parentElement.style.display='none';
 }
-// ****
-
-chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      url = tabs[0].url;
-      if (url == "https://www.youtube.com/"){
-        notify();
-      }
-   }
-);
-
-
-
+document.getElementById('add').addEventListener('click', addWebsite);
+document.getElementsByClassName('green alert').addEventListener('click', closeAlert)
